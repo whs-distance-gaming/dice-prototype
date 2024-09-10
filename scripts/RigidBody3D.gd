ada61d0c473eef0959232f1d76948fb08966b5d1
Jona <jona-pohlmann@gmx.de>
2024-09-10 18:40:08 +0200

    Würfel Button

--- scripts/RigidBody3D.gd
+++ scripts/RigidBody3D.gd
@@ -11,25 +11,43 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Input.is_action_pressed("ui_accept") && sleeping:
+	if Input.is_action_pressed("ui_accept")&& sleeping:
 		randomize()
 		_roll_dice()
 
 func _roll_dice():
 	sleeping = false
-	var random_number = randi_range(-50,50)
-	while random_number < 20 and random_number > -20:
-		random_number = randi_range(-50, 50)
-	var random_force = Vector3(random_number, random_number, random_number)
-	var random_torque = Vector3(random_number, random_number, random_number)
+	
+	var random_force = get_random_force(-70, 70)
+	var random_torque = get_random_force(-70, 70)
 	
 	apply_impulse(Vector3(), random_force)
 	apply_torque_impulse(random_torque)
 	is_rolling = true
 	first_roll = false
 
+func get_random_force(min_value: float, max_value: float) -> Vector3:
+	var force = Vector3(
+		randi_range(min_value, max_value), 
+		randi_range(min_value, max_value), 
+		randi_range(min_value, max_value)
+	)
+	while force.length() < 20:
+		force = Vector3(
+			randi_range(min_value, max_value), 
+			randi_range(min_value, max_value), 
+			randi_range(min_value, max_value)
+		)
+	return force
+
+
 func _on_sleeping_state_changed():
 	if sleeping && !first_roll:
 		for raycast in raycasts:
 			if raycast.is_colliding():
 				roll_finished.emit(raycast.opposite_side)
+
+
+func _on_button_pressed():
+	randomize()
+	_roll_dice()
