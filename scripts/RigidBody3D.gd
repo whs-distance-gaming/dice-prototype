1d035663c7aa19d0901c9d7a1cd332e1560ebcf4
Jona <jona-pohlmann@gmx.de>
2024-09-25 18:35:37 +0200

    mehr würfel

--- scripts/RigidBody3D.gd
+++ scripts/RigidBody3D.gd
@@ -2,7 +2,7 @@ extends RigidBody3D
 
 var is_rolling = false
 var first_roll = true
-var pressed = false
+var value
 signal roll_finished(values)
 @onready var raycasts = $Raycasts.get_children()
 
@@ -12,22 +12,25 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if (Input.is_action_pressed("ui_accept") or pressed) and sleeping:
-		pressed = false
+	if Input.is_action_pressed("ui_accept") and sleeping:
 		randomize()
-		_roll_dice()
+		roll_dice()
 		
 func _on_button_roll_pressed() -> void:
-	pressed = true
+	var dices = get_parent().dice_list
+	if sleeping:
+		randomize()
+		for dice in dices:
+			dice.roll_dice()
 
-func _roll_dice():
-	sleeping = false
-	var random_force = get_random_force(-60, 60)
-	var random_torque = get_random_force(-60, 60)
-	apply_impulse(Vector3(), random_force)
-	apply_torque_impulse(random_torque)
-	is_rolling = true
-	first_roll = false
+func roll_dice():
+		sleeping = false
+		var random_force = get_random_force(-60, 60)
+		var random_torque = get_random_force(-60, 60)
+		apply_impulse(Vector3(), random_force)
+		apply_torque_impulse(random_torque)
+		is_rolling = true
+		first_roll = false
 
 func get_random_force(min_value: float, max_value: float) -> Vector3:
 	var force = Vector3(
@@ -47,6 +50,6 @@ func get_random_force(min_value: float, max_value: float) -> Vector3:
 func _on_sleeping_state_changed():
 	if sleeping && !first_roll:
 		for raycast in raycasts:
-			var value = raycast.opposite_side
+			value = raycast.opposite_side
 			if raycast.is_colliding():
 				roll_finished.emit(value)
--- scripts/world.gd
+++ scripts/world.gd
@@ -3,15 +3,20 @@ extends Node3D
 var result = 0
 var list: Array[int] = []
 var dices = 1
+var dice_list: Array = []
 var spin_value = 1
 var max_columns = 4
 var spacing = 3.0
 @onready var result_label = $CanvasLayer/Result
 
+func _ready() -> void:
+	var first_dice = $"Würfel"
+	dice_list.append(first_dice)
 
 func _on_würfel_roll_finished(values):
 	$CanvasLayer/SpinBox.editable = false
 	list.append(values)
+	print(list)
 	if dices == 1:
 		for item in list:
 			result += item
@@ -21,7 +26,7 @@ func _on_würfel_roll_finished(values):
 		dices = spin_value
 	else:
 		dices -= 1
-	
+
 func set_result_text(result):
 	result_label.text = str(result)
 	var tween = get_tree().create_tween()
@@ -30,7 +35,6 @@ func set_result_text(result):
 	await tween.finished
 	result_label.queue_free
 	result_label.text = ""
-
 
 func _on_spin_box_value_changed(value: float) -> void:
 	spin_value = value
@@ -42,15 +46,18 @@ func _on_spin_box_value_changed(value: float) -> void:
 			remove_dice()
 			
 func add_dice():
+	var new_dice = preload("res://scenes/würfel.tscn").instantiate()
 	var original = $"Würfel"
 	var copy = original.duplicate()
+	dice_list.append(copy)
 	add_child(copy)
 	copy.position = calculate_position(dices)
 	dices += 1
 
 func remove_dice():
-	var last_dice = get_child(get_child_count() - 1)
-	last_dice.queue_free()
+	if dice_list.size() > 0:
+		var last_dice = dice_list.pop_back()
+		last_dice.queue_free()
 	dices -= 1
 
 func calculate_position(index: int) -> Vector3:
